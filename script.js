5a6e37f3194fde7468bd28d0c16044b12fe84960
Diogo Martins <me.martinsfe@gmail.com>
2022-08-07 00:48:52 -0300

    feat: complete items 7 and 8

--- script.js
+++ script.js
@@ -2,10 +2,18 @@ const newTaskInput = document.querySelector('#texto-tarefa');
 const newTaskButton = document.querySelector('#criar-tarefa');
 const taskList = document.querySelector('#lista-tarefas');
 
+const clickSelect = (e) => {
+  const selectListItems = document.querySelectorAll('li');
+  for (let item of selectListItems) {
+    item.classList.remove('select');
+    e.target.classList.add('select');
+  }
+};
 
 const addTask = () => {
   const createTaskItem = document.createElement('LI');
   createTaskItem.innerText = newTaskInput.value;
+  createTaskItem.addEventListener('click', clickSelect)
   taskList.append(createTaskItem);
   newTaskInput.value = '';
 };
